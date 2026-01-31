1e00c9d6abba0fbc861d76b39a4a5ee42bd8e8c5
Harshit <harshitpandit3525@gmail.com>
2026-02-01 00:25:17 +0530

    added NodeJs Require vs Import example

---
+++ NodeJs/RequireVsImport/export.js
@@ -0,0 +1,2 @@
+export const pi=3.14;
+export const remainder = (a, b) => a % b;
---
+++ NodeJs/RequireVsImport/import.js
@@ -0,0 +1,4 @@
+import {remainder} from "./export.js"
+console.log("Your remainder is: ",remainder(235,19));
+import randomWord from 'random-word';
+console.log(randomWord())
