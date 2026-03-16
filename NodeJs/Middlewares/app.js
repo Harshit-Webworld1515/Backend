ee85bab3b27d9d0fc38ccdf6ab13e7d18526d6dc
Harshit <harshitpandit3525@gmail.com>
2026-03-16 23:51:13 +0530

    Error handling Middleware with custom classes

--- NodeJs/Middlewares/app.js
+++ NodeJs/Middlewares/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const ExpressError = require("./ExpressError");
 
 //our first middleware
 app.use((req, res, next) => {
@@ -19,37 +20,53 @@ app.use((req, res, next) => {
 });
 
 //exploring app.use() for specific path 
-// if we specify a path then it will only work for that path or its subpaths and not for other paths
-app.use("/random",(req,res,next)=>{
+// if we specify a path then it will only work for that path or its subpaths and not for other paths like http://localhost:8080/random/abc
+app.use("/random", (req, res, next) => {
     console.log("This will only work on /random path");
     next();
 })
-let checkToken=("/api",(req,res,next)=>{
+//This is also a middleware for /api path
+let checkToken = ("/api", (req, res, next) => {
     let { token } = req.query;
-    if(token !== "dhurandhar"){
-        return res.status(401).send("UNAUTHORIZED! ACCESS DENIED");
+    if (token !== "dhurandhar") {
+        throw new ExpressError("Invalid token given by the user", 401);
     }
     console.log("This will only work on /api path");
     next();
 })
-// if / root path passed, it will work for all the paths because all the paths start with /, but if we use /random then it will only work for /random path and not for other paths like /about or /contact
+// if / root path passed, it will work for all the paths because all the paths start with /, but if we use /random then it will only work for /random path and its subpaths like /random/abc
 
 app.get("/random", (req, res) => {
     res.send("Welcome to the random page!");
 })
 
-app.get("/api",checkToken, (req, res) => {
+app.get("/api", checkToken, (req, res) => {
+    console.log("This is our API route");
     res.send("Welcome to the API page!");
 })
 
 app.get("/", (req, res) => {
     res.send("Hello, World! from the root directory");
 })
+app.get("/err", (req, res) => {
+    abc=abc; // this will throw an error because abc is not defined
+    res.send("This will not be executed because of the error");
+})
+app.use((err, req, res, next) => {
+    console.log("Error1: Something went wrong!");
+    next(err); // Call the next error handler middleware in the stack
+});
+app.use((err, req, res, next) => {
+    console.log("Error2: Something went wrong!" + err.message);
+    next(err); // Call the next error handler middleware in the stack
+    res.send(err.message);
+});
+
 
 //exploring app.use() for 404 error handling
-app.use((req, res) => {
-    res.status(404).send("404 Page not found");
-})
+// app.use((req, res) => {
+//     res.status(404).send("404 Page not found");
+// })
 
 app.listen(8080, () => {
     console.log("server listening to port 8080");
