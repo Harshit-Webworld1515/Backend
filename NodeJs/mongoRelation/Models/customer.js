d106c7fc58dc9fa8186e6d48e38d638a02e5fec3
Harshit <harshitpandit3525@gmail.com>
2026-03-23 03:29:57 +0530

    Implement one-to-many relation between Customer and Order

---
+++ NodeJs/mongoRelation/Models/customer.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const { Schema, model, connect } = mongoose;
+
+async function main() {
+    await connect('mongodb://127.0.0.1:27017/relationDemo');
+    console.log('Connected to MongoDB');
+}
+main();
+
+// ---------------- ORDER ----------------
+const orderSchema = new Schema({
+    item: String,
+    price: Number
+});
+
+const Order = model("Order", orderSchema);
+
+// ---------------- CUSTOMER ----------------
+const CustomerSchema = new Schema({
+    name: String,
+    orders: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Order"
+        }
+    ]
+});
+
+const Customer = model("Customer", CustomerSchema);
+
+// ---------------- INSERT ORDERS ----------------
+const addOrder = async () => {
+    await Order.insertMany([
+        { item: "Samosa", price: 15 },
+        { item: "Kachori", price: 20 },
+        { item: "Jalebi", price: 30 }
+    ]);
+};
+
+// ---------------- CREATE CUSTOMER ----------------
+const addCustomer = async () => {
+    const cust1 = new Customer({
+        name: "Astha Mishra"
+    });
+
+    let orders = await Order.find({
+        item: { $in: ["Samosa", "Kachori"] }
+    });
+
+    // Push each order _id
+    orders.forEach(order => {
+        cust1.orders.push(order);
+    });
+
+    let result= await cust1.save();
+
+    console.log("Customer saved:", result);
+};
+
+// ---------------- RUN ----------------
+    addCustomer();
