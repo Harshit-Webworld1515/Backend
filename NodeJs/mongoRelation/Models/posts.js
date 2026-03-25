930c4c7d210ff87c6407b13d929394e5853d490d
Harshit <harshitpandit3525@gmail.com>
2026-03-25 16:26:52 +0530

    Enable automatic Cascading  cleanup of posts when user is deleted

--- NodeJs/mongoRelation/Models/posts.js
+++ NodeJs/mongoRelation/Models/posts.js
@@ -10,7 +10,7 @@ const userSchema= new Schema({
     name:String,
     email:String
 })
-const User = model( "User" , userSchema );
+
 const postSchema=new Schema({
     content:String,
     likes :Number,
@@ -19,20 +19,38 @@ const postSchema=new Schema({
         ref:"User"
     }
 })
+//Cascading Middleware for  deletion
+userSchema.post("findOneAndDelete",async(user)=>{
+     if (user) {
+        await Post.deleteMany({
+            users: user._id
+        });
+        console.log("Deleted Post by Middleware");
+    }
+})
+const User = model( "User" , userSchema );
 const Post= model("Post", postSchema);
 const addData= async()=>{
-    const user=await User.findOne({name:"Aman"})
+    const user2= await User.findOne({
+        name:"Mahrana Pratap",
+    })  
     const post2= new Post({
-        likes:33,
-        content:"My World of Programming!"
+        likes:60,
+        content:"Mharo Pyaro Rajsthan"
     })
-    post2.users=user;
-    // await user1.save();
+    post2.users=user2._id;  
+    // await user2.save();
     await post2.save();
+    console.log("Added");
 }
 // addData();
+
 const getUser= async()=>{
     const result= await Post.find().populate("users","name");
     console.log(result);
 }
 getUser();
+const deluser= async()=>{
+    await User.findByIdAndDelete("69c3aa95a6fc90c9b5a2bcb9");
+}
+// deluser();
